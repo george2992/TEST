c2559590423875f9dc0db4dcc5684c6b6f7e016a
Jorge David Mendoza Jiménez <jorge@nulldata.com>
2020-07-12 16:20:08 -0500

    migracion recor

---
+++ database/migrations/2020_07_12_192416_create_table_requests_record.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableRequestsRecord extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests_record', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->uuid('uuid_group');
+            $table->text('input');
+            $table->text('trace');
+            $table->char('http_status', 3)->nullable()->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requests_record');
+    }
+}
